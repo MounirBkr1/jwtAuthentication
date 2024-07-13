0d784a0e156a5fe3726b6081369635887462bd71
MounirBkr1 <miirbri1@gmail.com>
2024-07-13 06:10:17 -0400

    JwtUtils

---
+++ src/main/java/com/mnr/jwtauthentication/jwt/JwtUtils.java
@@ -0,0 +1,11 @@
+package com.mnr.jwtauthentication.jwt;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class JwtUtils {
+    private static final Logger logger= LoggerFactory.getLogger(JwtUtils.class)
+}
