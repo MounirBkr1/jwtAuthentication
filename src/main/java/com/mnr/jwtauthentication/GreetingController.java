a75a5b342ee61a79f9f27fe24360bbc857e2ba42
MounirBkr1 <miirbri1@gmail.com>
2024-07-13 06:12:41 -0400

    JwtUtils

--- src/main/java/com/mnr/jwtauthentication/GreetingController.java
+++ src/main/java/com/mnr/jwtauthentication/GreetingController.java
@@ -1,2 +1,32 @@
-package com.mnr.jwtauthentication;public class GreetingController {
+package com.mnr.jwtauthentication;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+
+    //restricted for user
+    @PreAuthorize("hasRole('USER')") //work before execution of this method
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "hello, user!";
+    }
+
+    //restricted for admin
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "hello, admin!";
+    }
+
+
 }
