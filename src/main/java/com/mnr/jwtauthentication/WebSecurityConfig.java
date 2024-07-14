f08f46e472e31523275e6eae75b4bafd243f7c44
MounirBkr1 <miirbri1@gmail.com>
2024-07-14 12:17:35 -0400

    sec Config 2

--- src/main/java/com/mnr/jwtauthentication/WebSecurityConfig.java
+++ src/main/java/com/mnr/jwtauthentication/WebSecurityConfig.java
@@ -1,2 +1,168 @@
-package com.mnr.jwtauthentication;public class WebSecurityConfig {
+package com.mnr.jwtauthentication;
+
+import com.mnr.jwtauthentication.jwt.AuthEntryPointJwt;
+import com.mnr.jwtauthentication.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
+//    @Autowired
+//    UserDetails userDetails;
+
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    @Order(2147483642)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authRequests) -> {
+            authRequests.requestMatchers("/h2-console/**").permitAll()
+                    .requestMatchers("/signin").permitAll()
+                            .anyRequest().authenticated();
+        });
+
+        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(exception-> exception.authenticationEntryPoint(unauthorizedHandler));
+        //http.formLogin(Customizer.withDefaults());
+        //http.httpBasic(Customizer.withDefaults());
+        //allow frame from h2 console
+
+        http.headers(headers ->
+                headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+
+
+        http.csrf(csrf-> csrf.disable());
+        http.addFilterBefore(authenticationJwtTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+
+        return (SecurityFilterChain)http.build();
+    }
+
+    public UserDetailsService userDetailsService(){
+        JdbcUserDetailsManager userDetailsManager
+                = new JdbcUserDetailsManager(dataSource);
+
+        UserDetails user1 = User.withUsername("user1")
+                //.password("{noop}123") //noop = not encode password
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                //.password("{noop}123") //noop = not coded
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+       return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public CommandLineRunner initData(UserDetailsService userDetailsService){
+        return args -> {
+            JdbcUserDetailsManager manager = (JdbcUserDetailsManager) userDetailsService;
+            UserDetails user1 = User.withUsername("user1")
+                    //.password("{noop}123") //noop = not encode password
+                    .password(passwordEncoder().encode("123"))
+                    .roles("USER")
+                    .build();
+
+            UserDetails admin = User.withUsername("admin")
+                    //.password("{noop}123") //noop = not coded
+                    .password(passwordEncoder().encode("123"))
+                    .roles("ADMIN")
+                    .build();
+            //IN MEMORY AUTHENTICATION
+            // return new InMemoryUserDetailsManager(user1,admin);
+
+
+            //H2 DATABASE AUTHENTICATION
+        };
+
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/test/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
--- src/main/java/com/mnr/jwtauthentication/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/mnr/jwtauthentication/jwt/AuthEntryPointJwt.java
@@ -1,2 +1,52 @@
-package com.mnr.jwtauthentication.jwt;public class AuthEntryPointJwt {
+package com.mnr.jwtauthentication.jwt;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+
+/*
+    provide custom handling for unauthorized requests,typically when
+     authentication is required but not supplied or valid.
+     when an unauthorized request is detected, it logs the error and returns
+     a JSON response with an error message;status code,and the path attemted.
+
+ */
+
+public class AuthEntryPointJwt implements AuthenticationEntryPoint{
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        System.out.println(authException);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+
 }
